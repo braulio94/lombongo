8a6964651160ab04c094deb2706629366c444eb3
Braulio Cassule <brauliocassule94@gmail.com>
2019-03-07 16:14:02 +0100

    Alert Dialog

--- lib/components/alert.dart
+++ lib/components/alert.dart
@@ -14,62 +14,39 @@ class _AlertComponentsState extends State<AlertComponents> {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: <Widget>[
-        //este é o botão para adicionar novas rendas
-        Row(
+      children: [
+        Tab(text: 'Renda', icon: Icon(Icons.attach_money)),
+        Tab(text: 'Despesa', icon: Icon(Icons.money_off)),
+        Tab(text: 'Categoria', icon: Icon(Icons.dashboard)),
+      ].map((menu){
+        return Row(
           children: <Widget>[
             Padding(
               padding: EdgeInsets.all(1.0),
-              child: Icon(Icons.attach_money),
+              child: menu.icon,
             ),
             Padding(
               padding: EdgeInsets.all(4.0),
               child: MaterialButton(
                 onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => Renda()));
+                  Navigator.pop(context);
+                  Navigator.push(context, MaterialPageRoute(builder: (context){
+                    switch(menu.text){
+                      case 'Renda':
+                        return Renda();
+                      case 'Despesa':
+                        return Despesa();
+                      case 'Categoria':
+                        return Categoria();
+                    }
+                  }));
                 },
-                child: Text('Renda'),
+                child: Text(menu.text),
               ),
             ),
           ],
-        ),
-        //este é o botão para adicionar despesas
-        Row(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(1.0),
-              child: Icon(Icons.money_off),
-            ),
-            Padding(
-              padding: EdgeInsets.all(4.0),
-              child: MaterialButton(
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => Despesa()));
-                },
-                child: Text('Despesa'),
-              ),
-            ),
-          ],
-        ),
-        //este é o botão para adicionar uma nova categoria
-        Row(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(1.0),
-              child: Icon(Icons.dashboard),
-            ),
-            Padding(
-              padding: EdgeInsets.all(4.0),
-              child: MaterialButton(
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => Categoria()));
-                },
-                child: Text('Categoria'),
-              ),
-            ),
-          ],
-        ),
-      ],
+        );
+      }).toList(),
     );
   }
 }
