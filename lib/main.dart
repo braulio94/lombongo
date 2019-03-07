14876d9b979f7cf47161fe4558d95413385a0b38
Braulio Cassule <brauliocassule94@gmail.com>
2019-03-07 15:30:50 +0100

    App title

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:lombongo/pages/home.dart';
-void main() => runApp(MaterialApp(
+
+void main() => runApp(
+    MaterialApp(
+      title: 'Lombongo',
       debugShowCheckedModeBanner: false,
       home: Home(),
-    ));
-
+    )
+);
